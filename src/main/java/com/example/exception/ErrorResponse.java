1c89b969971d8b8f2286696f19aa72c3d737db4c
Thiago Oliveira <thg.mnzs@gmail.com>
2023-10-09 20:29:09 -0300

    gitflow-feature-stash: error-handling

---
+++ src/main/java/com/example/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String code;
+    private String description;
+    private String message;
+}
